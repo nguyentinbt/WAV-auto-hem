28d225f39add86191bd52ef1cd6c057279ef4ad8
Minh Tu <minhtu11598@gmail.com>
2025-04-17 09:16:56 +0700

    add 5

--- tu-test.js
+++ tu-test.js
@@ -1,7 +1,8 @@
 function testFirstCommit(){
     console.log("Hello World");
-    console.log("Hello World - 2");
+    console.log("Hello World -2");
     console.log("Hello World -3");
+    console.log("Hello World -5");
 }
 
 function testSecondCommit(){
