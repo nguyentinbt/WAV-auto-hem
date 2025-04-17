decdeaa87e5154efd2ab635ec3760c95f300330b
Minh Tu <56148969+minhtu11598@users.noreply.github.com>
2025-04-17 09:15:07 +0700

    Update tu-test.js sv

--- tu-test.js
+++ tu-test.js
@@ -1,6 +1,7 @@
 function testFirstCommit(){
     console.log("Hello World");
-    console.log("Hello World");
+    console.log("Hello World - 2");
+    console.log("Hello World -3");
 }
 
 function testSecondCommit(){
